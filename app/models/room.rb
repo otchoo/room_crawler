d68f633d17cda6492d60b1290ce86977b75f4e6f
nguyenluc312 <nguyenluc312@gmail.com>
2017-03-19 11:10:51 +0700

    init-models

---
+++ app/models/room.rb
@@ -0,0 +1,29 @@
+require 'simple_enum/mongoid'
+
+class Room
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include SimpleEnum::Mongoid
+
+  field :room_number, type: String
+  field :area, type: String
+  field :address, type: String
+  field :direction, type: String
+  field :number_of_bedrooms, type: String
+  field :number_of_bathrooms, type: String
+  field :project, type: String
+  field :floor, type: String
+  field :utilities, type: String
+  field :environment, type: String
+  field :description, type: String
+  field :price_per_metre_square, type: String
+  field :price, type: String
+  field :images, type: String
+  field :city, type: String
+  field :district, type: String
+  field :provider_site, type: Integer
+  field :code, type: String
+  field :provider_url, type: String
+
+  as_enum :provider_site, ["muabannhadat.vn", "nhadat24h.net"]
+end
